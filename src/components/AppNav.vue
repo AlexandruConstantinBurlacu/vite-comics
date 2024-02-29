18800b91cd2a37149675cd8c8f6485cd06f2aad6
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-29 14:45:43 +0100

    inserimento logo e diverse categorie attraverso un array

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,6 +1,17 @@
 <script>
   export default {
     name: 'AppNav',
+
+    data() {
+    return {
+        categories: [
+        "CHARACTERS", "COMICS", "MOVIES", "TV",
+        "GAMES", "COLLECTIBLES", "VIDEOS", "FANS",
+        "NEWS", "SHOP"
+        ],
+    }
+  }
+
   }
 
 </script>
@@ -9,11 +20,29 @@
 
 <template>
     
+    <nav>
+        <div class="logo">
+            <img src="\vue-dc-comics-1\vue-dc-comics-1\img\dc-logo.png" alt="DC Logo">
+        </div>
+
+        <ul class="selection">
+            <li v-for="currentCategory in categories">
+                {{ currentCategory }}
+            </li>
+        </ul>
+    </nav>
+
+
 </template>
 
 
 
 <style lang="scss">
+
+nav {
+    display: flex;
+    justify-content: center;
+}
 
 
 </style>
