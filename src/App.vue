b00fd98b92b6636cb7d25d2eef610f7b37ca5b7b
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-29 14:20:33 +0100

    collegamento appNav con app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script>
 
+  import AppNav from './components/AppNav.vue';
+
+
+  export default {
+    components: {
+      AppNav,
+    }
+  }
+
 </script>
+
+
 
 <template>
 
+  <AppNav></AppNav>
+
+
 </template>
 
-<style>
+
+
+<style lang="scss">
+
+  @use 'src/styles/general.scss' as *;
 
 </style>
